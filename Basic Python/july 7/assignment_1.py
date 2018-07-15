db8f0d08d4f767dd1885098c166f72f0ae4e99cf
nitisha13 <40908447+nitisha13@users.noreply.github.com>
2018-07-15 08:50:53 +0545

    Assignment 1

---
+++ Basic Python/july 7/assignment_1.py
@@ -0,0 +1,21 @@
+#question 1
+v=25
+u=0
+t=10
+a = (v-u)/t
+print(a)
+
+#question 2
+f_name = input ("enter first name")
+l_name=input("enter last name")
+full_name=f_name+""+l_name
+print(full_name)
+
+#question 3
+user_input = input("enter something")
+print(user_input * 10)
+
+#question 4
+print (type(4))
+print (str(45))
+print(eval("2+3"))
